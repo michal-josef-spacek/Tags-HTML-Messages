2560c379057183d2d518a9ac63091139c7c7a174
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-06-12 10:58:27 +0200

    Prepare for 0.05 version

--- Messages.pm
+++ Messages.pm
@@ -8,7 +8,7 @@ use Class::Utils qw(set_params split_params);
 use Error::Pure qw(err);
 use Scalar::Util qw(blessed);
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Constructor.
 sub new {
@@ -310,6 +310,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
